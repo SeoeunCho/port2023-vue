fed7d7e784c2aa05677f8961119e3ea457d7f43b
seoeuncho <westsilver24@naver.com>
2023-09-16 15:56:39 +0900

    contact

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,3 +121,32 @@ export const portText = [
     name: "메가박스 반응형 사이트",
   },
 ];
+
+export const contactText = [
+  {
+    link: "",
+    title: "Phone : 010-8577-4978",
+  },
+  {
+    link: "mailto:jowestsilver@gmail.com",
+    title: "mail : jowestsilver@gmail.com",
+  },
+];
+
+export const footerText = [
+  {
+    title: "resume",
+    desc: "노션으로 작성한 이력서를 확인할 수 있습니다. 경력기술서도 첨부되어 있습니다.",
+    link: "https://enchanting-drip-66d.notion.site/661e000688d04abf94686ae02d8983ff?pvs=4",
+  },
+  {
+    title: "github",
+    desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+    link: "https://github.com/SeoeunCho",
+  },
+  {
+    title: "portfolio",
+    desc: "처음으로 제작한 포트폴리오 사이트를 둘러보세요.",
+    link: "https://SeoeunCho.github.io/Portfolio01/",
+  },
+];
